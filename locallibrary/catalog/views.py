94cb51fddb71f8da6b85ff7217527e440ad61461
Guy Chaplin <guy.chaplin@googlemail.com>
2024-01-25 12:08:44 +0000

    Add All Borrowed view with permissions

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -71,3 +71,19 @@ class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
             .filter(status__exact='o')
             .order_by('due_back')
         )
+    
+
+from django.contrib.auth.mixins import PermissionRequiredMixin
+
+class LoanedBooksListView(PermissionRequiredMixin, generic.ListView):
+    """Generic class-based view listing books on loan."""
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed.html'
+    paginate_by = 10
+    permission_required = 'catalog.can_mark_returned'
+
+    def get_queryset(self):
+        return (
+            BookInstance.objects.filter(status__exact='o').order_by('due_back')
+        )
+    
